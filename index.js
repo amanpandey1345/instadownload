8c789b39c9ee0077420b3eb3afccb30025d9fbbd
amanpandey1345 <70140546+amanpandey1345@users.noreply.github.com>
2024-01-10 15:48:20 +0530

    ashg

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: ["http://localhost:3000", "https://opencodes.vercel.app/"],
     //   credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
     })
